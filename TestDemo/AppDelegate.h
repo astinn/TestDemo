977ec5e6f2f5bbe2d5dc342bd19138cd171ede1b
Abhishek Upadhyay <AstinTech@abhisheks-mac-mini.local>
2016-01-18 16:55:05 +0530

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Abhishek Upadhyay on 1/18/16.
+//  Copyright © 2016 Astin Technology Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by Abhishek Upadhyay on 1/18/16.
+//  Copyright © 2016 Astin Technology Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
