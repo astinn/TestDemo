6b534137bff5aef57111e90d4c2551e60983d1ac
astinn <naveen.s@astintechnology.com>
2016-01-18 18:22:28 +0530

    change bg color
    change bg color

--- TestDemo/TestViewController.m
+++ TestDemo/TestViewController.m
@@ -13,6 +13,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view setBackgroundColor:[UIColor redColor]];
+    
 }
 
 - (void)didReceiveMemoryWarning {
